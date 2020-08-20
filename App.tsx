85ad74b138260407a324ab04401bb8503b563138
kajetanz <kajetanz@wp.pl>
2020-08-20 10:25:02 +0200

    add coinpaprika API

--- App.tsx
+++ App.tsx
@@ -1,30 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CoinsScreen from './CoinsScreen';
@@ -48,8 +22,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-});
 
 export default App;
--- CoinsScreen.tsx
+++ CoinsScreen.tsx
@@ -4,7 +4,18 @@ import {
   View,
   Text
 } from 'react-native';
+const CoinpaprikaAPI = require('@coinpaprika/api-nodejs-client');
 
+const client = new CoinpaprikaAPI();
+
+let coins: JSON = {};
+client.getTicker().then(res => {
+  console.log(res.length);
+  coins = res;
+}).catch(console.error);
+// client.getGlobal().then(console.log).catch(console.error);
+
+console.log('aaa')
 const CoinsScreen = () => {
   return (
     <View>
