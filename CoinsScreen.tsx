f1864096ab59e3692e029c3b759ad755099a33de
kajetanz <kajetanz@wp.pl>
2020-08-20 10:14:41 +0200

    add bottom navigation to project

---
+++ CoinsScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+const CoinsScreen = () => {
+  return (
+    <View>
+      <Text>Coins</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default CoinsScreen;
---
+++ TodayScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+const TodayScreen = () => {
+  return (
+    <View>
+      <Text>Today</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default TodayScreen;
